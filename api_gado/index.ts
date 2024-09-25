f619ef61525fac81d68550450bb3de74553ab7b7
LeontinoMad <leontinomadruga@hotmail.com>
2024-09-25 06:25:32 -0300

    Adicionando Clientes

--- api_gado/index.ts
+++ api_gado/index.ts
@@ -2,14 +2,17 @@ import express from "express";
 import cors from "cors";
 import gadosRoutes from "./routes/gados";
 import racasRoutes from "./routes/racas";
+import clientesRotes from "./routes/clientes";
 
 const app = express();
 const port = 3004;
 
 app.use(express.json());
 app.use(cors());
+
 app.use("/gados", gadosRoutes);
 app.use("/racas", racasRoutes);
+app.use("/clientes", clientesRotes);
 
 app.get("/", (req, res) => {
   res.send("API : Sistema de venda de gado");
